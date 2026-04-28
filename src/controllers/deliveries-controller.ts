e7c382b66df3093a7664f6f7da48b9878335086a
viniciusrbr <viniciusgoes159@gmail.com>
2026-04-27 22:34:51 -0300

    feat: enhance deliveries controller with create and index methods for delivery management

--- src/controllers/deliveries-controller.ts
+++ src/controllers/deliveries-controller.ts
@@ -1,8 +1,33 @@
 import { Request, Response } from 'express';
+import z from 'zod';
+import { prisma } from '@/database/prisma';
 
 class DeliveriesController {
-  create(req: Request, res: Response) {
-    return res.json({ message: 'Hello World' });
+  async create(req: Request, res: Response) {
+    const bodySchema = z.object({
+      user_id: z.string().uuid(),
+      description: z.string()
+    });
+
+    const { user_id, description } = bodySchema.parse(req.body);
+
+    await prisma.delivery.create({
+      data: {
+        userId: user_id,
+        description,
+      },
+    });
+
+    return res.status(201).json();
+  }
+
+  async index(req: Request, res: Response) {
+    const deliveries = await prisma.delivery.findMany({
+      include: {
+        user: { select: { name: true, email: true } },
+      },
+    });
+    return res.json(deliveries);
   }
 }
 
